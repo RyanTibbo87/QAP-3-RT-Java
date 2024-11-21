c6099f1bb4b4c46af1dcf9c8446b02ebdcecc381
RyanTibbo87 <Ryan.Tibbo@keyin.com>
2024-11-21 06:14:11 -0330

    getters and setters for person

--- Problem1/Person.java
+++ Problem1/Person.java
@@ -1,1 +1,45 @@
 package Problem1;
+
+public class Person {
+    protected String myName; // Name of the person
+    protected int myAge;     // Age of the person
+    protected String myGender; // Gender ("M" for male, "F" for female)
+
+    // Constructor
+    public Person(String name, int age, String gender) {
+        myName = name;
+        myAge = age;
+        myGender = gender;
+    }
+
+    // Getters and Setters
+    public String getName() {
+        return myName;
+    }
+
+    public void setName(String name) {
+        myName = name;
+    }
+
+    public int getAge() {
+        return myAge;
+    }
+
+    public void setAge(int age) {
+        myAge = age;
+    }
+
+    public String getGender() {
+        return myGender;
+    }
+
+    public void setGender(String gender) {
+        myGender = gender;
+    }
+
+    // toString Method
+    public String toString() {
+        return myName + ", age: " + myAge + ", gender: " + myGender;
+    }
+}
+
