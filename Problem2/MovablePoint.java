889a9afcb02d94a2ff2bfcbd6f8944cfea477cea
RyanTibbo87 <Ryan.Tibbo@keyin.com>
2024-11-21 06:58:36 -0330

    getters/setters/constructor for movablepoint

--- Problem2/MovablePoint.java
+++ Problem2/MovablePoint.java
@@ -1,5 +1,67 @@
 package Problem2;
 
-public class MovablePoint {
-    
+public class MovablePoint extends Point {
+    private float xSpeed = 0.0f;
+    private float ySpeed = 0.0f;
+
+    // Default Constructor
+    public MovablePoint() {
+    }
+
+    // Constructor with parameters
+    public MovablePoint(float x, float y, float xSpeed, float ySpeed) {
+        super(x, y); // Call the parent class constructor
+        this.xSpeed = xSpeed;
+        this.ySpeed = ySpeed;
+    }
+
+    // Constructor with xSpeed and ySpeed only
+    public MovablePoint(float xSpeed, float ySpeed) {
+        this.xSpeed = xSpeed;
+        this.ySpeed = ySpeed;
+    }
+
+    // Getter for xSpeed
+    public float getXSpeed() {
+        return xSpeed;
+    }
+
+    // Setter for xSpeed
+    public void setXSpeed(float xSpeed) {
+        this.xSpeed = xSpeed;
+    }
+
+    // Getter for ySpeed
+    public float getYSpeed() {
+        return ySpeed;
+    }
+
+    // Setter for ySpeed
+    public void setYSpeed(float ySpeed) {
+        this.ySpeed = ySpeed;
+    }
+
+    // Setter for both xSpeed and ySpeed
+    public void setSpeed(float xSpeed, float ySpeed) {
+        this.xSpeed = xSpeed;
+        this.ySpeed = ySpeed;
+    }
+
+    // Getter for xSpeed and ySpeed as an array
+    public float[] getSpeed() {
+        return new float[]{xSpeed, ySpeed};
+    }
+
+    // Overridden toString() method
+    @Override
+    public String toString() {
+        return super.toString() + ",speed=(" + xSpeed + "," + ySpeed + ")";
+    }
+
+    // Method to move the point
+    public MovablePoint move() {
+        setX(getX() + xSpeed);
+        setY(getY() + ySpeed);
+        return this;
+    }
 }
