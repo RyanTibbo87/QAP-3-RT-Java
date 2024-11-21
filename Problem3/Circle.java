591c217c9602d00e2caaaf5108647784822ee42c
RyanTibbo87 <Ryan.Tibbo@keyin.com>
2024-11-21 07:06:09 -0330

    circle constructor

--- Problem3/Circle.java
+++ Problem3/Circle.java
@@ -1,5 +1,9 @@
 package Problem3;
 
-public class Circle {
-    
+public class Circle extends Ellipse {
+
+    // Constructor for Circle (uses Ellipse constructor)
+    public Circle(String name, double radius) {
+        super(name, radius, radius);
+    }
 }
