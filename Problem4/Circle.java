698fab6ddfc2bef3a14faa7f146ec66099fb794e
RyanTibbo87 <Ryan.Tibbo@keyin.com>
2024-11-21 07:12:47 -0330

    Circle constructor

--- Problem4/Circle.java
+++ Problem4/Circle.java
@@ -1,5 +1,11 @@
 package Problem4;
 
-public class Circle {
-    
+public class Circle extends Ellipse {
+
+    // Constructor for Circle (uses Ellipse constructor)
+    public Circle(String name, double radius) {
+        super(name, radius, radius);
+    }
+
+    // No additional scaling logic needed as Circle relies on Ellipse's scaling
 }
