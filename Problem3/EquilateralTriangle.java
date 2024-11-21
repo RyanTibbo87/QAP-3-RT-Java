7d479ace59a090e0187dce9732bcd2afcd97b056
RyanTibbo87 <Ryan.Tibbo@keyin.com>
2024-11-21 07:07:01 -0330

    equilateral triangle constructor

--- Problem3/EquilateralTriangle.java
+++ Problem3/EquilateralTriangle.java
@@ -1,5 +1,9 @@
 package Problem3;
 
-public class EquilateralTriangle {
-    
+public class EquilateralTriangle extends Triangle {
+
+    // Constructor
+    public EquilateralTriangle(String name, double side) {
+        super(name, side, side, side);
+    }
 }
