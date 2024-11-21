5ee9ed1511f02067a0f34c51d1c9ee83d20e2687
RyanTibbo87 <Ryan.Tibbo@keyin.com>
2024-11-21 06:15:23 -0330

    getters and setters for student

--- Problem1/Student.java
+++ Problem1/Student.java
@@ -1,4 +1,35 @@
 package Problem1;
-public class Student {
-    
+
+public class Student extends Person {
+    protected String myIdNum; // Student ID number
+    protected double myGPA;   // Grade point average
+
+    // Constructor
+    public Student(String name, int age, String gender, String idNum, double gpa) {
+        super(name, age, gender); // Call the constructor of the Person superclass
+        myIdNum = idNum;
+        myGPA = gpa;
+    }
+
+    // Getters and Setters
+    public String getIdNum() {
+        return myIdNum;
+    }
+
+    public void setIdNum(String idNum) {
+        myIdNum = idNum;
+    }
+
+    public double getGPA() {
+        return myGPA;
+    }
+
+    public void setGPA(double gpa) {
+        myGPA = gpa;
+    }
+
+    // toString Method
+    public String toString() {
+        return super.toString() + ", ID: " + myIdNum + ", GPA: " + myGPA;
+    }
 }
