b88e3d620f77ee2406c22ef0d44f8c9d47a789f6
RyanTibbo87 <Ryan.Tibbo@keyin.com>
2024-11-21 07:05:08 -0330

    abstract class shape getter and methods

--- Problem3/Shape.java
+++ Problem3/Shape.java
@@ -1,5 +1,25 @@
 package Problem3;
 
-public class Shape {
-    
+public abstract class Shape {
+    private String name;
+
+    // Constructor
+    public Shape(String name) {
+        this.name = name;
+    }
+
+    // Getter for name
+    public String getName() {
+        return name;
+    }
+
+    // Abstract methods for area and perimeter
+    public abstract double getArea();
+    public abstract double getPerimeter();
+
+    // Overridden toString method
+    @Override
+    public String toString() {
+        return "Shape: " + name + ", Area: " + getArea() + ", Perimeter: " + getPerimeter();
+    }
 }
